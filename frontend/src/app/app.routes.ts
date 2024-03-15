dda1eec39a3eadc79b420c5c24394fef3f76e8e5
Rob de Willigen <robdewilligen@outlook.com>
2024-03-15 13:20:09 +0100

    Add wildcard route and Redirect route
    added a wildcard route for pages not found and a redirect for /list towards the pathless main page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,22 +2,33 @@ import { Routes } from '@angular/router';
 import { AppComponent } from "./app.component";
 import { ListComponent } from "./list/list.component";
 import { DetailsComponent } from "./details/details.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
-export const routes: Routes = [
-    {
-        path: '',
-        component: AppComponent,
-        children: [
-            {
-                path: '',
-                title: 'Task list',
-                component: ListComponent
-            },
-            {
-                path: 'details/:id',
-                title: 'Task details',
-                component: DetailsComponent
-            }
-        ]
-    },
-];
+export const routes: Routes =
+    [
+        {
+            path: '',
+            component: AppComponent,
+            children: [
+                {
+                    path: '',
+                    title: 'Task list',
+                    component: ListComponent
+                },
+                {
+                    path: 'details/:id',
+                    title: 'Task details',
+                    component: DetailsComponent
+                }
+            ]
+        },
+        {
+            path: 'list',
+            redirectTo: '',
+            pathMatch: 'full'
+        },
+        {   // Wildcard route, should always be last in this order!
+            path: '**',
+            component: NotFoundComponent
+        }
+    ];

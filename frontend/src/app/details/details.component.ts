964cc96bf4abd7a9aa2043837dd4757805cb184c
Rob de Willigen <robdewilligen@outlook.com>
2024-03-20 14:40:53 +0100

    fix styling and update NotFound page

--- frontend/src/app/details/details.component.ts
+++ frontend/src/app/details/details.component.ts
@@ -1,34 +1,70 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { TaskService } from "../tasks.service";
 import { Tasks as Task } from '../interfaces/task.interface'
+import { NgIf } from "@angular/common";
 
 @Component({
     selector: 'ng-details',
     templateUrl: './details.component.html',
     styleUrl: './details.component.scss',
-    imports: [],
+    imports: [
+        NgIf
+    ],
     providers: [],
     standalone: true
 })
 export class DetailsComponent {
     title = 'details';
     task: any;
+    public isLoading: boolean = true;
 
     constructor(
         private route: ActivatedRoute,
+        private router: Router,
         private taskService: TaskService,
     ) {
         this.task = [];
     }
 
     ngOnInit() {
+        this.reload();
+        this.task = this.task[0];
+
+    }
+
+    getTaskData() {
         const taskId = parseInt(<string>this.route.snapshot.paramMap.get('id'))
         this.taskService.getTask(taskId).subscribe((data) => {
             console.log('data', data);
 
             this.task = data as Task[];
         });
-        this.task = this.task[0];
+    }
+
+    reload() {
+        this.getTaskData();
+        this.isLoading = true;
+        setTimeout(() => {
+            this.isLoading = false;
+        }, 150)
+    }
+
+    completeTask(id: number, completed: boolean) {
+        this.taskService.completeTask(id, completed).subscribe((data) => {
+            // console.log(data);
+
+            this.reload();
+        });
+    }
+
+    // DELETE Tasks
+    deleteTask(id: number) {
+        this.taskService.deleteTask(id).subscribe((data) => {
+            console.log(data);
+
+            this.reload();
+            this.router.navigate(['']);
+        });
     }
 }
